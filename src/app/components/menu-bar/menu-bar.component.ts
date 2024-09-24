b23c476a325f8a1d54649f88afdaed55281ae5d8
gabrielszg <gabrielsouza5@hotmail.com>
2024-09-24 11:53:13 -0300

    feat: added icon to menu items

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin, faGithub, faYoutube, IconDefinition } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
-
+  faLinkedin: IconDefinition = faLinkedin;
+  faGithub: IconDefinition = faGithub;
+  faYoutube: IconDefinition = faYoutube;
 }
